bcc88659d02ed388c221506fa719fd452d56b596
steisaden <steisaden@macbookpro.home>
2026-02-20 12:27:33 +0100

    fix(routing): scroll to top on page navigation

--- src/app/pages/RootLayout.tsx
+++ src/app/pages/RootLayout.tsx
@@ -1,10 +1,22 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
+import { useEffect } from "react";
 import { Navigation } from "../components/Navigation";
 import { Footer } from "../components/Footer";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 export default function RootLayout() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navigation />
       <main className="flex-1 pt-20">
         <Outlet />
